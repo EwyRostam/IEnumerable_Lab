7a9d4ec2b3874bf92a25f47fd3921501ef074894
Ewy Rostam <ewy.rost@gmail.com>
2024-02-07 09:00:59 +0100

    Created lab

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+
+IEnumerable exampleArray = new int[]{850, 2, 3};
+
+IEnumerator enumerator = exampleArray.GetEnumerator();
+
+while(enumerator.MoveNext()){
+
+Console.WriteLine(enumerator.MoveNext());
+Console.WriteLine(enumerator.Current);
+}
+
+enumerator.Reset(); //Use of this function??
+
+//The foreach loop is the exact same as the above
+
+foreach(var Current in exampleArray)
+{
+    Console.WriteLine(Current);
+}
